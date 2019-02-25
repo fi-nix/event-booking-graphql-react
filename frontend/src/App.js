98e2f62126b5ac160e8b96a421e1234266d0d7a8
Rajthilak <rajthilak.yuva@gmail.com>
2019-02-26 03:54:15 +0530

    Added nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,25 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/navigation/MainNavigation'
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/bookings" component={BookingsPage} />
-          <Route path="/events" component={EventsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/bookings" component={BookingsPage} />
+              <Route path="/events" component={EventsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
